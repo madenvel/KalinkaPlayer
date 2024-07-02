60cfe3f69d9c5d557ad5a5e22c65e5eb7302289d
MadEnvel <envelsavinds@gmail.com>
2024-07-02 23:51:34 +0100

    Unify logging format

--- run_server.py
+++ run_server.py
@@ -5,9 +5,51 @@ import uvicorn
 from src.config import config
 from src.netutils import get_ip_address
 
+
+uvicorn_log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s.%(msecs)03d %(levelname)s %(name)s: %(message)s",
+            "datefmt": "%H:%M:%S",
+        }
+    },
+    "handlers": {
+        "default": {
+            "level": "INFO",
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {
+        "uvicorn": {
+            "handlers": ["default"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "uvicorn.error": {
+            "level": "INFO",
+            "handlers": ["default"],
+            "propagate": False,
+        },
+        "uvicorn.access": {
+            "level": "INFO",
+            "handlers": ["default"],
+            "propagate": False,
+        },
+    },
+}
+
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s.%(msecs)03d %(levelname)s %(name)s: %(message)s",
+        datefmt="%H:%M:%S",
+    )
     host = get_ip_address(config["server"]["interface"])
     port = config["server"]["port"]
     logger.info(f"Starting server on {host}:{port}")
@@ -16,4 +58,5 @@ if __name__ == "__main__":
         host=host,
         port=port,
         reload=False,
+        log_config=uvicorn_log_config,
     )
