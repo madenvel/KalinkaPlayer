9a9afffecd98d78b9a426ce927615201dd1920e1
Dmitry Savin <envelsavinds@gmail.com>
2024-07-09 23:27:04 +0100

    Update comments

--- native_player/AudioGraphNode.h
+++ native_player/AudioGraphNode.h
@@ -17,16 +17,25 @@ class AudioGraphNode {
 public:
   AudioGraphNode() : state(AudioGraphNodeState::STOPPED) {}
 
+  // Get inner node state
   virtual StreamState getState();
 
+  // Set the callback to be called when the state changes.
+  // Returns the id of the callback.
   virtual int
   onStateChange(std::function<bool(AudioGraphNode *, StreamState)> callback);
+
+  // Remove the callback with the given id.
   virtual void removeStateChangeCallback(int id);
 
+  // Accept the source change. This is called when the source has changed and
+  // the reader should accept the new source.
+  // It has no effect if the node is not in SOURCE_CHANGED state.
   virtual void acceptSourceChange() {};
   virtual ~AudioGraphNode();
 
 protected:
+  // To be used by the derived classes to set the state.
   void setState(const StreamState &newState);
 
 private:
@@ -41,10 +50,13 @@ private:
 class AudioGraphOutputNode : public virtual AudioGraphNode {
 public:
   // Reads the data to the buffer and returns the number of bytes read.
-  // Sets the state to FINISHED if there's no more data expected.
-  // Note: the state may need additiona `read` call to be set to FINISHED,
-  //       Even though the buffer is already empty.
+  // Sets the state to FINISHED when all data has been read.
   virtual size_t read(void *data, size_t size) = 0;
+
+  // Waits for data to be available and returns the number of bytes available.
+  // The size parameter is the minimum number of bytes to wait for.
+  // The function can return earlier if the stopToken is set or the source has
+  // changed.
   virtual size_t waitForData(std::stop_token stopToken = std::stop_token(),
                              size_t size = 1) = 0;
   virtual ~AudioGraphOutputNode() = default;
@@ -52,7 +64,10 @@ public:
 
 class AudioGraphInputNode : public virtual AudioGraphNode {
 public:
+  // Connect to reading node to the source of the data
   virtual void connectTo(std::shared_ptr<AudioGraphOutputNode> outputNode) = 0;
+
+  // Disconnect previously connected node
   virtual void disconnect(std::shared_ptr<AudioGraphOutputNode> outputNode) = 0;
   virtual ~AudioGraphInputNode() = default;
 };
