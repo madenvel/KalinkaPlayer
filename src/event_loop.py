a789031c56dce929eb0b4abf4578afbf1e50809a
Dmitry Savin <envelsavinds@gmail.com>
2024-08-06 22:30:43 +0100

    Support returning value via future in async loop

--- src/event_loop.py
+++ src/event_loop.py
@@ -1,15 +1,35 @@
 import logging
 from queue import Queue
+import threading
 
 from src.rpiasync import AsyncLoop
 
 
-def enqueue(func):
-    def async_task(self, *args, **kwargs):
-        def task():
-            func(self, *args, **kwargs)
+class Future:
+    def __init__(self):
+        self._done = threading.Event()
+        self._result = None
 
-        self.enqueue(task)
+    def set(self, result):
+        self._result = result
+        self._done.set()
+
+    def get(self):
+        self._done.wait()
+        return self._result
+
+    def done(self):
+        return self._done.is_set()
+
+
+def enqueue(func):
+    def async_task(self, *args, **kwargs) -> Future:
+        def task():
+            return func(self, *args, **kwargs)
+
+        future = Future()
+        self.enqueue((task, future))
+        return future
 
     return async_task
 
@@ -20,7 +40,7 @@ class AsyncExecutor(AsyncLoop):
 
     def process(self, e):
         if e is not None:
-            e()
+            e[1].set(e[0]())
         else:
             logging.warn("Not a function")
 
