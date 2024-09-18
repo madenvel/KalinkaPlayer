60976db092c5d440a36870ee5be7033bdc2f151f
Dmitry Savin <envelsavinds@gmail.com>
2024-09-18 22:12:35 +0100

    Add simple performance monitor

---
+++ native_player/PerfMon.cpp
@@ -0,0 +1,72 @@
+#include "PerfMon.h"
+
+#include <iostream>
+#include <numeric>
+
+PerfMon::PerfMon() {}
+
+void PerfMon::begin(const std::string &mark) {
+  auto &perfMark = marks[mark];
+  if (perfMark.open) {
+    return;
+  }
+
+  perfMark.open = true;
+  perfMark.point = std::chrono::high_resolution_clock::now();
+}
+
+void PerfMon::end(const std::string &mark) {
+  auto now = std::chrono::high_resolution_clock::now();
+  auto &perfMark = marks[mark];
+  if (!perfMark.open) {
+    return;
+  }
+
+  auto duration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(now - perfMark.point)
+          .count();
+
+  if (perfMark.durations.full()) {
+    auto size = perfMark.durations.capacity();
+    perfMark.averageNs +=
+        (double)(duration - perfMark.durations.front()) / size;
+    perfMark.durations.pop_front();
+  } else {
+    perfMark.averageNs =
+        (perfMark.averageNs * perfMark.durations.size() + duration) /
+        (perfMark.durations.size() + 1);
+  }
+
+  perfMark.durations.push_back(duration);
+  perfMark.open = false;
+}
+
+void PerfMon::printStats() {
+  for (const auto &mark : marks) {
+    std::cout << "PerfMon: " << mark.first << " average "
+              << mark.second.averageNs / 1000.0 << "us" << std::endl;
+  }
+}
+
+void PerfMon::printPeriodically(int seconds) {
+  if (printThread.joinable()) {
+    printThread.request_stop();
+    printThread.join();
+  }
+
+  printThread = std::jthread([this, seconds](std::stop_token token) {
+    while (!token.stop_requested()) {
+      std::this_thread::sleep_for(std::chrono::seconds(seconds));
+      printStats();
+    }
+  });
+}
+
+PerfMon::~PerfMon() {
+  if (printThread.joinable()) {
+    printThread.request_stop();
+    printThread.join();
+  }
+
+  printStats();
+}
---
+++ native_player/PerfMon.h
@@ -0,0 +1,59 @@
+#ifndef PERF_MON_H
+#define PERF_MON_H
+
+#include <boost/circular_buffer.hpp>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <unordered_map>
+
+#ifndef RELEASE
+#define perfmon_begin(mark) PerfMon::getInstance().begin(mark)
+#define perfmon_end(mark) PerfMon::getInstance().end(mark)
+#define perfmon_print_stats() PerfMon::getInstance().printStats()
+#define perfmon_print_periodically(seconds)                                    \
+  PerfMon::getInstance().printPeriodically(seconds)
+#else
+#define perfmon_begin(mark)
+#define perfmon_end(mark)
+#define perfmon_print_stats()
+#define perfmon_print_periodically(seconds)
+#endif
+
+#ifndef RELEASE
+class PerfMon {
+public:
+  static PerfMon &getInstance() {
+    static PerfMon instance;
+    return instance;
+  }
+
+  void begin(const std::string &mark);
+  void end(const std::string &mark);
+
+  void printStats();
+  void printPeriodically(int seconds);
+
+  double getAverageNs(const std::string &mark) { return marks[mark].averageNs; }
+
+private:
+  struct PerfMark {
+    std::chrono::high_resolution_clock::time_point point;
+    bool open;
+    double averageNs;
+    boost::circular_buffer<long> durations;
+
+    PerfMark() : open(false), averageNs(0.0), durations(10) {}
+  };
+  std::unordered_map<std::string, PerfMark> marks;
+
+  std::jthread printThread;
+
+  PerfMon();
+  ~PerfMon();
+  PerfMon(const PerfMon &) = delete;
+  PerfMon &operator=(const PerfMon &) = delete;
+};
+
+#endif // RELEASE
+#endif // PERF_MON_H
