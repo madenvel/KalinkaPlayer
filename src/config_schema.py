3af0826e6a7606e0e115cf5e4bd4bba0540573d8
Dmitry Savin <envelsavinds@gmail.com>
2025-02-27 00:09:57 +0000

    Support saving config and restarting the server via http

--- src/config_schema.py
+++ src/config_schema.py
@@ -1,30 +1,38 @@
 schema = """
 {
     "type": "section",
+    "name": "Kalinka",
     "description": "Kalinka configuration",
     "required": "yes",
     "elements": {
         "server": {
-            "description": "Server configuration",
+            "name": "Server",
+            "description": "Name, interface and port of the server",
             "type": "section",
             "required": "yes",
             "elements": {
                 "interface": {
+                    "name": "Interface",
                     "description": "Network interface to bind",
                     "type": "string",
-                    "required": "yes"
+                    "required": "yes",
+                    "readonly": "yes"
                 },
                 "port": {
+                    "name": "Port",
                     "description": "Port number to listen on",
                     "type": "integer",
-                    "required": "yes"
+                    "required": "yes",
+                    "readonly": "yes"
                 },
                 "service_name": {
+                    "name": "Service name",
                     "description": "Name of the service",
                     "type": "string",
                     "required": "yes"
                 },
                 "log_level": {
+                    "name": "Log level",
                     "description": "Logging level",
                     "type": "enum",
                     "values": ["debug", "info", "warning", "error"],
@@ -34,27 +42,32 @@ schema = """
             }
         },
         "output": {
+            "name": "Output",
             "description": "Output configuration",
             "type": "section",
             "required": "yes",
             "elements": {
                 "alsa": {
+                    "name": "ALSA",
                     "description": "ALSA output settings",
                     "type": "section",
                     "required": "yes",
                     "elements": {
                         "device": {
+                            "name": "Device",
                             "description": "ALSA device name",
                             "type": "string",
                             "required": "yes"
                         },
                         "latency_ms": {
+                            "name": "Latency",
                             "description": "Output latency in milliseconds",
                             "type": "integer",
                             "required": "no",
                             "default": 160
                         },
                         "period_ms": {
+                            "name": "Period",
                             "description": "Output period in milliseconds",
                             "type": "integer",
                             "required": "no",
@@ -65,21 +78,25 @@ schema = """
             }
         },
         "input": {
+            "name": "Input",
             "description": "Input configuration",
             "type": "section",
             "elements": {
                 "http": {
+                    "name": "HTTP",
                     "description": "HTTP input settings",
                     "type": "section",
                     "elements": {
                         "buffer_size": {
-                            "description": "HTTP buffer size",
+                            "name": "Buffer size",
+                            "description": "HTTP buffer size in bytes",
                             "type": "integer",
                             "required": "no",
                             "default": 384000
                         },
                         "chunk_size": {
-                            "description": "HTTP chunk size",
+                            "name": "Chunk size",
+                            "description": "HTTP chunk size in bytes",
                             "type": "integer",
                             "required": "no",
                             "default": 768000
@@ -89,17 +106,20 @@ schema = """
             }
         },
         "decoder": {
-            "description": "Decoder configuration",
+            "name": "Decoder",
+            "description": "Decoders configuration",
             "type": "section",
             "required": "yes",
             "elements": {
                 "flac": {
+                    "name": "FLAC",
                     "description": "FLAC decoder settings",
                     "type": "section",
                     "required": "no",
                     "elements": {
                         "buffer_size": {
-                            "description": "FLAC buffer size",
+                            "name": "Buffer size",
+                            "description": "FLAC buffer size in bytes",
                             "type": "integer",
                             "required": "no"
                         }
@@ -108,17 +128,20 @@ schema = """
             }
         },
         "fixups": {
-            "description": "Fixup settings",
+            "name": "Fixups",
+            "description": "Hacks to work around hardware issues",
             "type": "section",
             "required": "no",
             "elements": {
                 "alsa_sleep_after_format_setup_ms": {
+                    "name": "Sleep after new format",
                     "description": "ALSA sleep time after format setup in milliseconds",
                     "type": "integer",
                     "required": "no",
                     "default": 0
                 },
                 "alsa_reopen_device_with_new_format": {
+                    "name": "Always reopen device",
                     "description": "Reopen ALSA device with new format",
                     "type": "boolean",
                     "required": "no",
@@ -127,26 +150,31 @@ schema = """
             }
         },
         "addons": {
+            "name": "Addons",
             "description": "Addons configuration",
             "type": "section",
             "required": "yes",
             "elements": {
                 "device": {
+                    "name": "Devices",
                     "description": "Device-specific addons",
                     "type": "section",
                     "required": "yes",
                     "elements": {
                         "alsamixer": {
+                            "name": "ALSA mixer",
                             "description": "ALSA mixer settings",
                             "type": "section",
                             "required": "no",
                             "elements": {
                                 "name": {
+                                    "name": "Name",
                                     "description": "Mixer control name",
                                     "type": "string",
                                     "required": "yes"
                                 },
                                 "volume_step_to_db": {
+                                    "name": "Volume step",
                                     "description": "Volume step in dB",
                                     "type": "number",
                                     "required": "yes"
@@ -154,22 +182,26 @@ schema = """
                             }
                         },
                         "musiccast": {
+                            "name": "MusicCast",
                             "description": "MusicCast device settings",
                             "type": "section",
                             "required": "no",
                             "elements": {
                                 "device_addr": {
+                                    "name": "Address",
                                     "description": "MusicCast device address",
                                     "type": "string",
                                     "required": "yes"
                                 },
                                 "device_port": {
+                                    "name": "Port",
                                     "description": "MusicCast device port",
                                     "type": "integer",
                                     "required": "yes"
                                 },
                                 "connected_input": {
-                                    "description": "Connected input type",
+                                    "name": "Connected input",
+                                    "description": "Connected input name",
                                     "type": "string",
                                     "required": "yes"
                                 }
@@ -178,22 +210,26 @@ schema = """
                     }
                 },
                 "input_module": {
+                    "name": "Input",
                     "description": "Input module settings",
                     "type": "section",
                     "required": "yes",
                     "elements": {
                         "qobuz": {
+                            "name": "Qobuz",
                             "description": "Qobuz input module settings",
                             "type": "section",
                             "required": "no",
                             "elements": {
                                 "email": {
-                                    "description": "Qobuz account email",
+                                    "name": "Email",
+                                    "description": "Qobuz login",
                                     "type": "string",
                                     "required": "yes"
                                 },
                                 "password_hash": {
-                                    "description": "Qobuz account password hash",
+                                    "name": "Password",
+                                    "description": "Qobuz password",
                                     "type": "password",
                                     "required": "yes"
                                 }
