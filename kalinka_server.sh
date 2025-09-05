adb03bf7ca299da5c0177b4e973e042ceea1cf74
Dmitry Savin <envelsavinds@gmail.com>
2025-09-05 23:00:06 +0100

    Fix startup script

--- kalinka_server.sh
+++ kalinka_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/kalinka/venv/bin/activate
-exec python3 /opt/kalinka/run_server.py --config /etc/kalinka/kalinka_conf.cfg --state $HOME/.local/share/kalinka/kalinka_state.json
+exec kalinka-server --config /etc/kalinka/kalinka_conf.cfg --state $HOME/.local/share/kalinka/kalinka_state.json
