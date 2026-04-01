5110afa7437528ff183511f96dcd80ece9b7790e
Dmitry Savin <envelsavinds@gmail.com>
2026-04-01 23:30:27 +0100

    Remove kalinka_queued

--- packages/kalinka-server/src/kalinka_queued/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Queued - A generic async command queue with decorator support.
-"""
-
-from kalinka_queued.queue import CommandQueue, Command
-from kalinka_queued.decorators import queued, queued_class
-
-__version__ = "0.1.0"
-__all__ = ["CommandQueue", "Command", "queued", "queued_class"]
